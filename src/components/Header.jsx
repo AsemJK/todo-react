a6f54eb764d1f1b923cfd9986e8dc51fb81260e9
AsemJK <asemalsaiyadi@gmail.com>
2024-11-07 06:36:02 +0300

    ...

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <header>
+      <a href="/Home">Home</a>
+      <a href="/About">About</a>
+      <a href="/Contact">Contact</a>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <img src="/public/images/logo-dark.png" alt="" />
+      <Header />
+    </div>
+  );
+};
+
+export default Navbar;
